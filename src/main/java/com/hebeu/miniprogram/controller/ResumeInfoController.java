5c19d4b01c96d803db6fadc3945e940f62a4f0d1
baitianyu <1748383809@qq.com>
2022-05-12 22:51:56 +0800

    second commit

--- src/main/java/com/hebeu/miniprogram/controller/ResumeInfoController.java
+++ src/main/java/com/hebeu/miniprogram/controller/ResumeInfoController.java
@@ -1,15 +1,31 @@
 package com.hebeu.miniprogram.controller;
 
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.constant.WxMaConstants;
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.extension.api.R;
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+import com.hebeu.miniprogram.config.WxMaConfiguration;
 import com.hebeu.miniprogram.entity.ResumeInfo;
 import com.hebeu.miniprogram.service.ResumeInfoService;
 import com.hebeu.miniprogram.status.ServiceStatus;
+import me.chanjar.weixin.common.bean.result.WxMediaUploadResult;
+import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.print.DocFlavor;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -22,67 +38,92 @@ public class ResumeInfoController {
     @GetMapping("/delete_resume_info")
     public String deleteResumeInfo(int resumeId) {
         int i = resumeInfoService.deleteResumeInfo(resumeId);
-        if (i!=0)
+        if (i != 0)
             return ServiceStatus.DELETE_RESUME_INFO_SUCCESS;
         else
             return ServiceStatus.DELETE_RESUME_INFO_FAILED;
     }
 
     @GetMapping("/insert_resume_info")
-    public String insertResumeInfo() {
-        return null;
+    public List<String> insertResumeInfo(String appid, HttpServletRequest request, int userId, int recruit_id) {
+        final WxMaService wxService = WxMaConfiguration.getMaService(appid);
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (!resolver.isMultipart(request)) {
+            return Lists.newArrayList();
+        }
+        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+        Iterator<String> it = multiRequest.getFileNames();
+        List<String> result = Lists.newArrayList();
+        while (it.hasNext()) {
+            try {
+                MultipartFile file = multiRequest.getFile(it.next());
+                File newFile = new File(Files.createTempDir(), file.getOriginalFilename());
+                file.transferTo(newFile);
+                ResumeInfo resumeInfo = new ResumeInfo();
+                resumeInfo.setUserId(userId);
+                resumeInfo.setResumeStatus(ServiceStatus.RESUME_POSTED);
+                resumeInfo.setResumeFileUrl(newFile.toString());
+                resumeInfo.setRecruitId(recruit_id);
+                resumeInfoService.insertResumeInfo(resumeInfo);
+                WxMediaUploadResult uploadResult = wxService.getMediaService().uploadMedia(WxMaConstants.KefuMsgType.IMAGE, newFile);
+                result.add(uploadResult.getMediaId());
+            } catch (IOException | WxErrorException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     @GetMapping("/search_all_resume_info")
-    public String searchAllResumeInfo(){
+    public String searchAllResumeInfo() {
         List<ResumeInfo> resumeInfos = resumeInfoService.searchAllResumeInfo();
-        if (resumeInfos!=null)
+        if (resumeInfos != null)
             return JSON.toJSONString(resumeInfos);
         else
             return ServiceStatus.SEARCH_ALL_RESUME_INFO_FAILED;
     }
 
     @GetMapping("/search_resume_info_by_user_id")
-    public String searchResumeInfoByUserId(int userId){
+    public String searchResumeInfoByUserId(int userId) {
         List<ResumeInfo> resumeInfos = resumeInfoService.searchResumeInfoByUserId(userId);
-        if (resumeInfos!=null)
+        if (resumeInfos != null)
             return JSON.toJSONString(resumeInfos);
         else
             return ServiceStatus.SEARCH_RESUME_INFO_BY_USER_ID_FAILED;
     }
 
     @GetMapping("/search_resume_info_by_resume_id")
-    public String searchResumeInfoByResumeId(int resumeId){
+    public String searchResumeInfoByResumeId(int resumeId) {
         ResumeInfo resumeInfo = resumeInfoService.searchResumeInfoByResumeId(resumeId);
-        if (resumeInfo!=null)
+        if (resumeInfo != null)
             return JSON.toJSONString(resumeInfo);
         else
             return ServiceStatus.SEARCH_RESUME_INFO_BY_RESUME_ID_FAILED;
     }
 
     @GetMapping("/search_resume_info_by_recruit_id")
-    public String searchResumeInfoByRecruitId(int recruitId){
+    public String searchResumeInfoByRecruitId(int recruitId) {
         List<ResumeInfo> resumeInfos = resumeInfoService.searchResumeInfoByRecruitId(recruitId);
-        if (resumeInfos!=null)
+        if (resumeInfos != null)
             return JSON.toJSONString(resumeInfos);
         else
             return ServiceStatus.SEARCH_RESUME_INFO_BY_RECRUIT_ID_FAILED;
     }
 
     @GetMapping("/update_resume_info_status")
-    public String updateResumeInfoStatus(String resumeStatus, int resumeId){
+    public String updateResumeInfoStatus(String resumeStatus, int resumeId) {
         int i = resumeInfoService.updateResumeInfoStatus(resumeStatus, resumeId);
-        if (i!=0)
+        if (i != 0)
             return ServiceStatus.UPDATE_RESUME_INFO_STATUS_SUCCESS;
         else
             return ServiceStatus.UPDATE_RESUME_INFO_STATUS_FAILED;
     }
 
     @GetMapping("/update_resume_info")
-    public String updateResumeInfo(String JSON_resumeInfo){
-        ResumeInfo resumeInfo=JSON.parseObject(JSON_resumeInfo,ResumeInfo.class);
+    public String updateResumeInfo(String JSON_resumeInfo) {
+        ResumeInfo resumeInfo = JSON.parseObject(JSON_resumeInfo, ResumeInfo.class);
         int i = resumeInfoService.updateResumeInfo(resumeInfo);
-        if (i!=0)
+        if (i != 0)
             return ServiceStatus.UPDATE_RESUME_INFO_SUCCESS;
         else
             return ServiceStatus.UPDATE_RESUME_INFO_FAILED;
